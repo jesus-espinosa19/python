a72d02f4d55a398878dd102d74b284a5633fbd36
jesus-espinosa19 <jesusarroyo1064@gmail.com>
2025-04-24 09:18:51 -0500

    Add files via upload

---
+++ clase.py
@@ -0,0 +1,44 @@
+x = ["manzana", "limon"]
+
+print("manzana" in x)
+x = min(2, 10, 100)
+y = max(3, 10, 200)
+
+print(x)
+print(y)
+
+x = min(1, 2, 300)
+y = max(3, 6, 500)
+
+print(x)
+print(y)
+
+print("")
+
+print("valor absoluto")
+x = abs(-6.25)
+print(x)
+print("")
+print("potencia de y")
+x = pow(4,2)
+print(x)
+print("")
+
+
+import math
+x = math.sqrt(64)
+print(x)
+print("")
+
+import math
+x = math.ceil(2.4)
+y = math.floor(5.6)
+
+print(x)
+print(y)
+print("")
+
+import math
+x = math.pi
+print(x)
+
